fb9252415e089540a0006cd35a72beb69fae197d
a24caisu <a24caisu@student.his.se>
2025-11-26 09:26:24 +0100

    Cage Script
    Kommer inte längre än så med den här jävla musen

---
+++ Mycelia/Assets/assets/scripts/CageOpen.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class CageOpen : MonoBehaviour
+{
+    private bool isOpen = false;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isOpen)
+        {
+            
+        }
+    }
+}
