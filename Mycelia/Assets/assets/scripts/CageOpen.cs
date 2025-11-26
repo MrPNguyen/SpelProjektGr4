0150a4b4670dbf426928156de8f2d08997efca81
a24caisu <a24caisu@student.his.se>
2025-11-26 11:05:22 +0100

    DoorOpenScript

--- Mycelia/Assets/assets/scripts/CageOpen.cs
+++ Mycelia/Assets/assets/scripts/CageOpen.cs
@@ -1,19 +1,33 @@
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
 
 public class CageOpen : MonoBehaviour
 {
-    private bool isOpen = false;
+    public bool IsOpen;
+    private Animator doorAnimator;
+    private Collider2D other;
     void Start()
     {
-        
+        //doorAnimator = GetComponent<Animator>();
+        //doorAnimator.SetBool.("Open", false)
+        other = GetComponent<Collider2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (isOpen)
+        if (IsOpen && other.gameObject.tag == "Player")
         {
-            
+            //doorAnimator.SetBool("isOpen", true);
+            Console.WriteLine("isOpen");
         }
     }
+
+    public void OpenDoor()
+    {
+        IsOpen = true;
+    }
+
 }
