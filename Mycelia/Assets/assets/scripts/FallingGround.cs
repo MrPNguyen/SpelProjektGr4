d057c55942d3fc1c7a52906916702e8302198e50
a24caisu <a24caisu@student.his.se>
2025-11-27 10:54:52 +0100

    Falling ground

--- Mycelia/Assets/assets/scripts/FallingGround.cs
+++ Mycelia/Assets/assets/scripts/FallingGround.cs
@@ -1,49 +1,65 @@
 using System.Collections;
 using UnityEngine;
+using System.Collections.Generic;
 
 public class FallingGround : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private Rigidbody2D rb;
-    [SerializeField] private Vector3 posB;
-    [SerializeField] private float Seconds;
-    private float SecondsPassed = 0;
+    [SerializeField] private float posB;
+    [SerializeField] private float WaitTime;
     private Vector3 posA;
     private bool falling;
+
+
+
+
     void Start()
     {
         posA = transform.position;
-        
+        rb = GetComponent<Rigidbody2D>();
+
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
-        
+
+        float i = 0;
         if (falling)
         {
-            if (SecondsPassed < Seconds)
-            {
-                SecondsPassed += Time.deltaTime;
-                float t = SecondsPassed / Seconds; //Normaliserar värder till [o, 1]
-                transform.position = Vector3.Lerp(posA, posB, t);
-            }
-            if (SecondsPassed < Seconds)
-            {
-                SecondsPassed += Time.deltaTime;
-                float t = SecondsPassed / Seconds; //Normaliserar värder till [o, 1]
-                transform.position = Vector3.Lerp(posB, posA, t);
-            }
-            falling = false;
+            rb.gravityScale = 1;
+            StartCoroutine(Wait(WaitTime));
+            
         }
     }
-
+    
     public void fall()
     {
-        Debug.Log("falling = true");
         falling = true;
     }
 
-
-    
+    IEnumerator Wait(float WaitTime)
+    {
+        yield return new WaitForSeconds(WaitTime);
+        if (transform.position.y < -5)
+        { if (falling)
+            {
+                rb.linearVelocityY = 0;
+                falling = false;
+            }
+            rb.gravityScale = -2;
+        }
+                
+        yield return new WaitForSeconds(WaitTime);
+                
+        if (transform.position.y >= posA.y)
+        {
+            rb.gravityScale = 0;
+            rb.linearVelocityY = 0;
+            
+        }
+        
+    }
 }
+
