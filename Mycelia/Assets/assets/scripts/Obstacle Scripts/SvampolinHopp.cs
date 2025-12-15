bafa73655ebc1ade351091bd43293de83e4823a1
Philip Nguyen <yetrexgamer@gmail.com>
2025-12-15 09:47:11 +0100

    Svampolin animationer
    Fixat så att svampolin animationer bara sker när man hoppar på dem... inte oändligt loopande.

--- Mycelia/Assets/assets/scripts/Obstacle Scripts/SvampolinHopp.cs
+++ Mycelia/Assets/assets/scripts/Obstacle Scripts/SvampolinHopp.cs
@@ -1,13 +1,16 @@
-using Unity.VisualScripting;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SvampolinHopp : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     [SerializeField] private PlayerMovement playerMovement;
     [SerializeField] private float Bounce;
-
-
+    [SerializeField] private Animator animator;
+    [SerializeField] private float Delay;
 
 
     public void OnTriggerEnter2D(Collider2D other)
@@ -16,12 +19,27 @@ public class SvampolinHopp : MonoBehaviour
         {
             if (playerMovement.multiplier >= 2)
             {
+                //BounceCoroutine(Delay);
+                animator.SetBool("isBounce", true);
                 playerMovement.hasHardDropped = false;
                 playerMovement.velocity.y = Bounce;
-           
             }
         
         }
     }
-
+    
+    public void OnTriggerExit2D(Collider2D other)
+    {
+        animator.SetBool("isBounce", false);
+    }
+    
+    /*private IEnumerator BounceCoroutine(float Delay)
+    {
+        Debug.Log("Bouncecoroutine");
+        animator.SetBool("isBounce", true);
+        playerMovement.hasHardDropped = false;
+        playerMovement.velocity.y = Bounce;
+        yield return new WaitForSeconds(Delay);
+        animator.SetBool("isBounce", false);
+    }*/
 }
