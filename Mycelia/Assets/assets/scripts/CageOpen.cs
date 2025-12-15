7a7374a4fcf3abea635f76044d3620a25448dddd
Philip Nguyen <yetrexgamer@gmail.com>
2025-12-15 13:01:15 +0100

    Nya sprites + börjat på slut
    Jag har lagt in nya sprites och börjat på avslutet på level1

--- Mycelia/Assets/assets/scripts/CageOpen.cs
+++ Mycelia/Assets/assets/scripts/CageOpen.cs
@@ -3,23 +3,26 @@ using System.Collections;
 using System.Collections.Generic;
 using System;
 using UnityEngine.Events;
+using UnityEngine.InputSystem;
 
 
 public class CageOpen : MonoBehaviour
 
 {
-    public UnityEvent onTriggerEnter;
+    //public UnityEvent onTriggerEnter;
 
     private bool IsOpen = false;
-    private Animator doorAnimator;
+    private bool isInRange = false;
+    [SerializeField] private Sprite openDoor;
+    private SpriteRenderer Sr;
+    [SerializeField] private PlayerManager player;
     
     
    
     private string tagToActivate = "Player";
     void Start()
     {
-        //doorAnimator = GetComponent<Animator>();
-        //doorAnimator.SetBool.("Open", false)
+        Sr = gameObject.GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -28,18 +31,29 @@ public class CageOpen : MonoBehaviour
         
     }
 
-     void OnTriggerEnter2D(Collider2D other)
+    /*void OnTriggerEnter2D(Collider2D other)
     {
-      
         if (IsOpen && other.CompareTag(tagToActivate))
         {  onTriggerEnter.Invoke();
-            //doorAnimator.SetBool("isOpen", true);
+            Sr.sprite = openDoor;
             Debug.Log("isOpen");
             GetComponent<BoxCollider2D>().isTrigger = false;
             
         }
+    }*/
 
-        
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        isInRange = true;
+    }
+    public void OpenCage(InputAction.CallbackContext context)
+    {
+        if (isInRange)
+        {
+            player.SaveKantarells();
+            Sr.sprite = openDoor;
+            GetComponent<BoxCollider2D>().isTrigger = false;
+        }
     }
 
     public void OpenDoor()
