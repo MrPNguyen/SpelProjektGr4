5d64c25cdf4afe4aafa9a6145f931e673ab2cab6
a24caisu <a24caisu@student.his.se>
2025-12-17 11:32:41 +0100

    Key-count + guiding walls

--- Mycelia/Assets/assets/scripts/Obstacle Scripts/SvampolinHopp.cs
+++ Mycelia/Assets/assets/scripts/Obstacle Scripts/SvampolinHopp.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.UI;
 
 public class SvampolinHopp : MonoBehaviour
@@ -30,6 +31,10 @@ public class SvampolinHopp : MonoBehaviour
             {
                 playerMovement.hasHardDropped = false;
                 playerMovement.velocity.y = 0.5f * MathF.Abs(playerMovement.velocity.y);
+            }
+            if (Keyboard.current.spaceKey.wasPressedThisFrame)
+            {
+                playerMovement.velocity.y = 6.5f;
             }
 
 
